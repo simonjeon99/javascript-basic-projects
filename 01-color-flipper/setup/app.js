123208b6167d94c4da647c1df9a3cfaac28d7508
Simon Jeon <simonjeon100@gmail.com>
2022-02-22 08:48:41 -0600

    color flipper and counter

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color"); //why .color
+
+btn.addEventListener("click", function () {
+    const randomnumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomnumber];
+    color.textContent = colors[randomnumber];
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
